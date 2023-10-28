d512ae63d8f469d8b12612d7a223d8543e879fb4
Aswin <aswinvijayan97@gmail.com>
2023-10-28 15:33:57 +0530

    PRINT

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,36 @@ import { parseMD } from './utils/md-utils'
 import { content } from './content'
 function App() {
   const [text, setText] = useState(content)
+  const [collapse, setCollapse] = useState(false)
+  const [printPreview, setPrintPreview] = useState(false)
 
   useEffect(() => {
+    if (printPreview) {
+      setTimeout(() => { window.print() }, 1000)
+      setTimeout(() => { setPrintPreview(false) }, 2000)
+    }
+  }, [printPreview])
 
-  }, [])
-  const print=()=>{
-
+  const print = () => {
+    setPrintPreview(true)
   }
   return (
     <>
       <div className='container'>
-        <div className='left'>
-          {/* {text} */}
+        {!collapse && !printPreview && <div className='view left'>
           <textarea className='input-area' type='text' value={text} onChange={(e) => {
             setText(e.target.value)
           }}></textarea>
-        </div>
-        <div className={'right markdown'} style={{ textAlign: 'left' }}>
-          <div className='preview-top-bar'>
-          <button onClick={print}>Print 🖨️</button>
-          </div>
+        </div>}
+        <div className={`right markdown ${collapse||printPreview ? "coll" : "exp"}`} style={{ textAlign: 'left' }}>
+          {!printPreview && <div className='view preview-top-bar'>
+            <button onClick={() => setCollapse(t => !t)}>{collapse ? "✍️" : "👁️"}</button>
+            <button onClick={print}>Print 🖨️</button>
+          </div>}
           <div className='content-class' id="content">
-          {
-            parseMD(text)
-          }
+            {
+              parseMD(text)
+            }
           </div>
         </div>
       </div>
